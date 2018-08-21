cae5c90cefa4cf2b851eeebfa4e3a5a511f3e84a
Mcanthony <mcdemilroc@gmail.com>
2018-08-21 17:54:55 +0530

    fixed database

--- movie-reviews/blogs/urls.py
+++ movie-reviews/blogs/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.conf.urls import url
 from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns,static
 from django.conf import settings
@@ -8,7 +9,7 @@ app_name = 'post'
 urlpatterns = [
     path('',views.MovieList.as_view(),name='list'),
     path('post_key/<int:pk>/',views.MovieDetailView.as_view(),name='detail_pk'),
-    path('post/<slug:slug>/',views.MovieDetailView.as_view(),name='detail'),
+    url(r'^post/(?P<slug>[-\w]+)/$',views.MovieDetailView.as_view(),name='detail'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
