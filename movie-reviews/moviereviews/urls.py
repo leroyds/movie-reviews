46eb3b635ee67bbc2e9ac314e8cf37e23b352207
Unknown <leroyds@live.com>
2018-08-21 16:36:49 +0530

    Authentication
    login,signup

--- movie-reviews/moviereviews/urls.py
+++ movie-reviews/moviereviews/urls.py
@@ -18,5 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('blogs.urls'))
+    path('',include('blogs.urls')),
+    path('account/',include('accounts.urls'))
 ]
