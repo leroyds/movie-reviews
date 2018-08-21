b54b0e6657cc0f3d4d3247a1f4c472ab997e6fee
Unknown <leroyds@live.com>
2018-08-21 15:11:42 +0530

    template restyle

--- movie-reviews/blogs/urls.py
+++ movie-reviews/blogs/urls.py
@@ -6,7 +6,7 @@ from django.conf import settings
 app_name = 'post'
 
 urlpatterns = [
-    path('',views.MovieList.as_view()),
+    path('',views.MovieList.as_view(),name='list'),
     path('post/<slug:slug>/',views.MovieDetailView.as_view(),name='detail')
 ]
 
